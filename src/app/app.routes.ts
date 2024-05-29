a1752b361eb7ebbde93ceb14f53b821ac62ce8ea
juan pablo JPR <128744584+juanpablor20@users.noreply.github.com>
2024-05-29 17:06:54 -0500

    se agrego el login y regiter

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,27 @@
+
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {
-    path: 'dashboard',
-    loadComponent: () => import('./dashboard/dashboard.component'),
-    children: [
-      {
-        path: 'change-detection',
-        title: 'Change Detection',
-        loadComponent: () =>
-          import(
-            './dashboard/pages/change-detection/change-detection.component'
-          ),
-      },
+  
+    {
+   path: '',
+   loadChildren: () => import('./auth/auth.routes').then(m => m.AUTH_ROUTES)
+    },
+    // {
+    //   path: 'dashboard',
+    //   loadChildren: () => import('./dashboard/dashboard.component').then(m. => matchMedia.ADMIN_routes)
+    // },
+  //   path: 'dashboard',
+  //   loadComponent: () => import('./dashboard/dashboard.component'),
+  //   children: [
+  //     {
+  //       path: 'change-detection',
+  //       title: 'Change Detection',
+  //       loadComponent: () =>
+  //         import(
+  //           './dashboard/pages/change-detection/change-detection.component'
+  //         ),
+  //     },
       {
         path: 'control-flow',
         title: 'Control Flow',
@@ -21,30 +30,30 @@ export const routes: Routes = [
             './dashboard/pages/control-flow/control-flow.component'
           ),
       },
-      {
-        path: 'defer-options',
-        title: 'Defer Options',
-        loadComponent: () =>
-          import(
-            './dashboard/pages/defer-options/defer-options.component'
-          ),
-      },
-      {
-        path: 'defer-views',
-        title: 'Defer Views',
-        loadComponent: () =>
-          import(
-            './dashboard/pages/defer-views/defer-views.component'
-          ),
-      },
-      {
-        path: 'user:id',
-        title: 'User',
-        loadComponent: () =>
-          import(
-            './dashboard/pages/user/user.component'
-          ),
-      },
+  //     {
+  //       path: 'defer-options',
+  //       title: 'Defer Options',
+  //       loadComponent: () =>
+  //         import(
+  //           './dashboard/pages/defer-options/defer-options.component'
+  //         ),
+  //     },
+  //     {
+  //       path: 'defer-views',
+  //       title: 'Defer Views',
+  //       loadComponent: () =>
+  //         import(
+  //           './dashboard/pages/defer-views/defer-views.component'
+  //         ),
+  //     },
+  //     {
+  //       path: 'user:id',
+  //       title: 'User',
+  //       loadComponent: () =>
+  //         import(
+  //           './dashboard/pages/user/user.component'
+  //         ),
+  //     },
 
       {
         path: 'users',
@@ -54,23 +63,23 @@ export const routes: Routes = [
             './dashboard/pages/users/users.component'
           ),
       },
-      {
-        path: 'view-transition',
-        title: 'Views Transition',
-        loadComponent: () =>
-          import(
-            './dashboard/pages/view-transition/view-transition.component'
-          ),
-      },
-      {
-      path: '', redirectTo: 'control-flow', pathMatch: 'full',
-      }
-    ],
-  },
+  //     {
+  //       path: 'view-transition',
+  //       title: 'Views Transition',
+  //       loadComponent: () =>
+  //         import(
+  //           './dashboard/pages/view-transition/view-transition.component'
+  //         ),
+  //     },
+  //     {
+  //     path: '', redirectTo: 'control-flow', pathMatch: 'full',
+  //     }
+  //   ],
+  // },
 
-  {
-    path: '',
-    redirectTo: '/dashboard',
-    pathMatch: 'full',
-  },
+  // // {
+  // //   path: '',
+  // //   redirectTo: '/dashboard',
+  // //   pathMatch: 'full',
+  // // },
 ];
